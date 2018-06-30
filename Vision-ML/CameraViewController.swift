bd17de185937c47b57d527585cc99091045fe660
Rehan Parkar <parkarcamp@gmail.com>
2018-06-30 18:36:53 -0500

    initial init- project set up

---
+++ Vision-ML/CameraViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CameraViewController.swift
+//  Vision-ML
+//
+//  Created by Rehan Parkar on 2018-06-30.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
