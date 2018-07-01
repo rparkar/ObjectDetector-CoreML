175516c4ce7f7e3d35adbf29f7befc1f0a2bcd35
Rehan Parkar <parkarcamp@gmail.com>
2018-06-30 20:07:01 -0500

    cameraView shows output from camera

--- Vision-ML/Controller/CameraViewController.swift
+++ Vision-ML/Controller/CameraViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
@@ -18,10 +19,58 @@ class CameraViewController: UIViewController {
     @IBOutlet weak var confidenceLabel: UILabel!
     @IBOutlet weak var roundedLabelsView: RoundedShadowView!
     
+    //variables
+    var captureSession : AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        previewLayer.frame = cameraView.bounds
+        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+
+            cameraOutput = AVCapturePhotoOutput()
+
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.addOutput(cameraOutput)
+            }
+
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+            previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+
+            cameraView.layer.addSublayer(previewLayer!)
+
+            captureSession.startRunning()
+
+        }
+        catch {
+            debugPrint(error)
+        }
     }
 
 
